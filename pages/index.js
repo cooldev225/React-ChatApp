05e26ca108e8067b0a75e359b1de9568593922f0
cooldev225 <developer225@hotmail.com>
2021-09-10 22:25:47 +0900

    init

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Fragment } from "react";
+import Landing from "./landing";
+
+export default function Home() {
+	return (
+		<Fragment>
+			<Landing />
+		</Fragment>
+	);
+}
---
+++ pages/landing/teamWorkModal.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Button, Modal, ModalBody, ModalFooter, ModalHeader } from 'reactstrap';
+
+const TeamWorkModal = ({ modal, toggle }) => {
+  return (
+    <>
+      <Modal
+        className='fade show add-popup video-popup'
+        isOpen={modal}
+        toggle={toggle}
+        centered
+        size="lg"
+      >
+        <ModalHeader toggle={toggle}>
+          <iframe
+            src="https://www.youtube.com//embed/7e90gBu4pas" allowFullScreen >
+          </iframe>
+        </ModalHeader>
+      </Modal>
+    </>
+  );
+};
+
+export default TeamWorkModal;
