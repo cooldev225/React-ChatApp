f4a16fe7974d49e2fbaf9908791267b459fcc346
cooldev225 <63242681+cooldev225@users.noreply.github.com>
2021-11-23 14:05:27 +0800

    sql root

--- socket/app.js
+++ socket/app.js
@@ -17,7 +17,7 @@ const io = require('socket.io')(server, {
 const db = mysql.createConnection({
     host: "localhost",
     user: "root",
-    // password: "",
+    password: "tempP@ss123",
     database: "ldahkumy_ojochat",
 });
 // const db = mysql.createConnection({
