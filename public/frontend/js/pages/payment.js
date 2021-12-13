28640ec6a7a231815d75e80cc5cdd667c60ddb4f
cooldev225 <63242681+cooldev225@users.noreply.github.com>
2021-12-13 21:06:22 +0800

    blur com

--- public/frontend/js/pages/payment.js
+++ public/frontend/js/pages/payment.js
@@ -31,51 +31,51 @@ $(document).ready(function() {
             totalPrice += Number(blurPrice);
         }
         $('#checkoutModal .total-price span:last-child').text(`$${totalPrice}`);
-        let messageId = $('#photo_item .modal-content').attr('key');
-        let photoId = $('#photo_item .modal-content').attr('photoId');
-        socket.emit('pay:photo', { photoId, selectedEmojis });
-        payWholePhotoPrice();
+        // let messageId = $('#photo_item .modal-content').attr('key');
+        // let photoId = $('#photo_item .modal-content').attr('photoId');
+        // socket.emit('pay:photo', { photoId, selectedEmojis });
+        // payWholePhotoPrice();
     });
-    // paypal.Button.render({
-    //     env: 'sandbox', // Or 'production'
-    //     style: {
-    //         size: 'medium',
-    //         color: 'gold',
-    //         shape: 'pill',
-    //     },
-    //     // Set up the payment:
-    //     // 1. Add a payment callback
-    //     payment: function(data, actions) {
-    //         // 2. Make a request to your server
-    //         return actions.request.post('/api/create-paypal-transaction', {
-    //                 "_token": "{{ csrf_token() }}",
-    //                 totalPrice
-    //             })
-    //             .then(function(res) {
-    //                 // 3. Return res.id from the response
-    //                 // console.log(res);
-    //                 return res.id
-    //             })
-    //     },
-    //     // Execute the payment:
-    //     // 1. Add an onAuthorize callback
-    //     onAuthorize: function(data, actions) {
-    //         // 2. Make a request to your server
-    //         return actions.request.post('/api/confirm-paypal-transaction', {
-    //                 "_token": "{{ csrf_token() }}",
-    //                 payment_id: data.paymentID,
-    //                 payer_id: data.payerID
-    //             })
-    //             .then(function(res) {
-    //                 let messageId = $('#photo_item .modal-content').attr('key');
-    //                 let photoId = $('#photo_item .modal-content').attr('photoId');
-    //                 socket.emit('pay:photo', { photoId, selectedEmojis });
-    //                 alert('Payment successfully done!!');
-    //                 payWholePhotoPrice();
+    paypal.Button.render({
+        env: 'sandbox', // Or 'production'
+        style: {
+            size: 'medium',
+            color: 'gold',
+            shape: 'pill',
+        },
+        // Set up the payment:
+        // 1. Add a payment callback
+        payment: function(data, actions) {
+            // 2. Make a request to your server
+            return actions.request.post('/api/create-paypal-transaction', {
+                    "_token": "{{ csrf_token() }}",
+                    totalPrice
+                })
+                .then(function(res) {
+                    // 3. Return res.id from the response
+                    // console.log(res);
+                    return res.id
+                })
+        },
+        // Execute the payment:
+        // 1. Add an onAuthorize callback
+        onAuthorize: function(data, actions) {
+            // 2. Make a request to your server
+            return actions.request.post('/api/confirm-paypal-transaction', {
+                    "_token": "{{ csrf_token() }}",
+                    payment_id: data.paymentID,
+                    payer_id: data.payerID
+                })
+                .then(function(res) {
+                    let messageId = $('#photo_item .modal-content').attr('key');
+                    let photoId = $('#photo_item .modal-content').attr('photoId');
+                    socket.emit('pay:photo', { photoId, selectedEmojis });
+                    payWholePhotoPrice();
+                    alert('Payment successfully done!!');
 
-    //                 // 3. Show the buyer a confirmation message.
-    //             })
-    //     }
-    // }, '#paypal-button')
+                    // 3. Show the buyer a confirmation message.
+                })
+        }
+    }, '#paypal-button')
 
 });
