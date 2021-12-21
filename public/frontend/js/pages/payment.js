f0eb16370b55663c680f330438746a8f2afd9318
cooldev225 <63242681+cooldev225@users.noreply.github.com>
2021-12-21 16:22:30 +0800

    balance pay

--- public/frontend/js/pages/payment.js
+++ public/frontend/js/pages/payment.js
@@ -31,44 +31,55 @@ $(document).ready(function() {
             totalPrice += Number(blurPrice);
         }
         $('#checkoutModal .total-price span:last-child').text(`$${totalPrice}`);
-
-        // tempAction();
-        paypal.Button.render({
-            env: 'sandbox', // Or 'production'
-            style: {
-                size: 'medium',
-                color: 'gold',
-                shape: 'pill',
-            },
-            // Set up the payment:
-            // 1. Add a payment callback
-            payment: function(data, actions) {
-                // 2. Make a request to your server
-                return actions.request.post('/api/create-paypal-transaction', {
-                        "_token": "{{ csrf_token() }}",
-                        totalPrice
-                    })
-                    .then(function(res) {
-                        // 3. Return res.id from the response
-                        // console.log(res);
-                        return res.id
-                    })
-            },
-            // Execute the payment:
-            // 1. Add an onAuthorize callback
-            onAuthorize: function(data, actions) {
-                // 2. Make a request to your server
-                return actions.request.post('/api/confirm-paypal-transaction', {
-                        "_token": "{{ csrf_token() }}",
-                        payment_id: data.paymentID,
-                        payer_id: data.payerID
-                    })
-                    .then(function(res) {
-                        tempAction();
-                        // 3. Show the buyer a confirmation message.
-                    })
-            }
-        }, '#paypal-button')
+        if (!$('#paypal-button div').length)
+            paypal.Button.render({
+                env: 'sandbox', // Or 'production'
+                style: {
+                    size: 'medium',
+                    color: 'gold',
+                    shape: 'pill',
+                },
+                // Set up the payment:
+                // 1. Add a payment callback
+                payment: function(data, actions) {
+                    // 2. Make a request to your server
+                    return actions.request.post('/api/create-paypal-transaction', {
+                            "_token": "{{ csrf_token() }}",
+                            totalPrice
+                        })
+                        .then(function(res) {
+                            // 3. Return res.id from the response
+                            // console.log(res);
+                            return res.id
+                        })
+                },
+                // Execute the payment:
+                // 1. Add an onAuthorize callback
+                onAuthorize: function(data, actions) {
+                    // 2. Make a request to your server
+                    return actions.request.post('/api/confirm-paypal-transaction', {
+                            "_token": "{{ csrf_token() }}",
+                            payment_id: data.paymentID,
+                            payer_id: data.payerID
+                        })
+                        .then(function(res) {
+                            tempAction();
+                            // 3. Show the buyer a confirmation message.
+                        })
+                }
+            }, '#paypal-button')
+    });
+    $('.payWithBalanceBtn').on('click', () => {
+        let userInfo = getCertainUserInfoById(currentUserId);
+        if (!totalPrice) {
+            alert('This is free photo');
+            return;
+        }
+        if (userInfo.balances >= totalPrice) {
+            tempAction();
+        } else {
+            alert('You have no enough balance. Please pay via Paypal or Card');
+        }
     });
 
 });
@@ -79,4 +90,6 @@ function tempAction() {
     let addBalance = totalPrice * 0.7;
     socket.emit('pay:photo', { photoId, selectedEmojis, addBalance });
     payWholePhotoPrice();
+    $('#checkoutModal').modal('hide');
+    alert('You paid Successfully');
 }
