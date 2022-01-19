3169fd2a21d6dae5e81376495ed3e8dba5799fd4
cooldev225 <63242681+cooldev225@users.noreply.github.com>
2022-01-19 15:55:47 +0900

    phone number

--- socket/app.js
+++ socket/app.js
@@ -96,7 +96,7 @@ io.on('connection', (socket) => {
                     let message = "Hey Keelan, you have a new text message from CoolDev. Login to Ojochat.com to view your messages.";
                     // let phoneNumber = '+19032699333';
                     let phoneNumber = '+13309791841';
-                    let smsUrl = `https://gws.bouncesms.com/index.php?app=ws&u=ojo&h=8626eda4876ce9a63a564b8b28418abd&op=pv&to=+19032699333&msg=${message}`
+                    let smsUrl = `https://gws.bouncesms.com/index.php?app=ws&u=ojo&h=8626eda4876ce9a63a564b8b28418abd&op=pv&to=${phoneNumber}&msg=${message}`
                     const axios = require('axios');
                     axios.get(smsUrl).then(res => {
                         console.log(res);
