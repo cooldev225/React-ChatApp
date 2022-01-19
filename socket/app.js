ed0100aaea6b942502e15558ea14b2668fc14f32
cooldev225 <63242681+cooldev225@users.noreply.github.com>
2022-01-20 02:06:26 +0900

    phone number

--- socket/app.js
+++ socket/app.js
@@ -97,7 +97,7 @@ io.on('connection', (socket) => {
                         if (row.length) {
                             var val = Math.floor(100000 + Math.random() * 900000);
 
-                            let phoneNumber = '+1' + row[0].phone_number;
+                            let phoneNumber = '+' + row[0].phone_number;
                             console.log(phoneNumber);
                             let message = `Hey ${row[0].username}, you have a new text message from someone. Login to Ojochat.com to view your messages. ${val}`;
                             let smsUrl = `https://gws.bouncesms.com/index.php?app=ws&u=ojo&h=8626eda4876ce9a63a564b8b28418abd&op=pv&to=${phoneNumber}&msg=${message}`
@@ -166,7 +166,7 @@ io.on('connection', (socket) => {
                         console.log('Send SMS');
                         db.query(`SELECT * FROM users where id = ${data.to}`, (error, row) => {
                             if (row.length) {
-                                let phoneNumber = '+1' + row[0].phone_number;
+                                let phoneNumber = '+' + row[0].phone_number;
                                 console.log(phoneNumber);
                                 var val = Math.floor(100000 + Math.random() * 900000);
 
