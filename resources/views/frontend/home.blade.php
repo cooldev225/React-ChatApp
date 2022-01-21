27e638b3541685796993e4b3f2b5b2a02f2bdaf2
cooldev225 <63242681+cooldev225@users.noreply.github.com>
2022-01-21 23:10:12 +0900

    paypal

--- resources/views/frontend/home.blade.php
+++ resources/views/frontend/home.blade.php
@@ -4019,7 +4019,7 @@
     </div>
     <script src="/frontend/js/pages/homepage.js"></script>
     <script src="/frontend/js/pages/requestpage.js"></script>
-    {{-- <script src="https://www.paypalobjects.com/api/checkout.js"></script> --}}
+    <script src="https://www.paypalobjects.com/api/checkout.js"></script>
 
     <script src="/frontend/js/pages/payment.js"></script>
     <script src="/frontend/js/pages/setting.js"></script>
