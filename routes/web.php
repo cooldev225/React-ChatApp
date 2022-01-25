287a7e7cd38414c9e97c67c1d173af4e4c23b2ba
cooldev225 <63242681+cooldev225@users.noreply.github.com>
2022-01-25 21:41:52 +0900

    updatge

--- routes/web.php
+++ routes/web.php
@@ -44,6 +44,7 @@ Route::group(['middleware' => ['login']], function () {
     Route::post('/stripe', 'StripeController@stripePost')->name('stripe.post');
     Route::post('/setting/setPhoneNumber', 'SettingController@setPhoneNumber');
     Route::post('/setting/getPhoneNumber', 'SettingController@getPhoneNumber');
+    Route::get('/get', 'SettingController@index');
 });
 
 Route::group(['middleware' => ['admin']], function () {
