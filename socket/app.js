5763d86f64412ff802b7c32a6c715dd7c2abf6b9
stardev96610 <63242681+cooldev225@users.noreply.github.com>
2022-02-23 09:34:21 +0900

    complete

--- socket/app.js
+++ socket/app.js
@@ -345,7 +345,7 @@ io.on('connection', (socket) => {
     });
     socket.on('test:SMS', data => {
         console.log(data);
-        let fullPhoneNumber = '+' + data.dialCode + data.phoneNumber.replace(/[^0-9]/g, '');
+        let fullPhoneNumber = data.dialCode + data.phoneNumber.replace(/[^0-9]/g, '');
         let message = `Hey, your mobile number ${data.phoneNumber} has been updated at OJO.`;
         if (type == 1) {
             var smsUrl = `https://app.centsms.app/services/send.php?key=52efd2c71f080fa8d775b2a5ae1bb03cbb599e2f&number=${fullPhoneNumber}&message=${message}&devices=%5B%2237%22%2C%2238%22%5D&type=sms&useRandomDevice=1&prioritize=1`;
@@ -353,8 +353,8 @@ io.on('connection', (socket) => {
             var smsUrl = `https://app.centsms.app/services/send.php?key=52efd2c71f080fa8d775b2a5ae1bb03cbb599e2f&number=${fullPhoneNumber}&message=${message}&devices=58&type=sms&prioritize=1`;
         }
         axios.get(smsUrl).then(res => {
-            console.log(smsUrl);
-            console.log(res);
+            console.log(res)
+            console.log(res.success);
             if (res.status == 200) {
                 console.log('OK');
                 let senderSocketId = user_socketMap.get(currentUserId.toString());
