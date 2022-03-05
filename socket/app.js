cbcb6cc2025eb66dfd3cb623a982f74e210555b8
cooldev225 <developer225@hotmail.com>
2022-03-05 23:05:18 +0900

    number span

--- socket/app.js
+++ socket/app.js
@@ -341,7 +341,7 @@ io.on('connection', (socket) => {
                 var fullPhoneNumber = data.dialCode + data.phoneNumber.replace(/[^0-9]/g, '');
             }
             if (spainish) {
-                var message = `Oye, tu numero de móvil ${data.phoneNumber} ha sido actualizado en OJO.`;
+                var message = `Oye, tu numero de movil ${data.phoneNumber} ha sido actualizado en OJO.`;
             } else {
                 var message = `Hey, your mobile number ${data.phoneNumber} has been updated at OJO.`;
             }
