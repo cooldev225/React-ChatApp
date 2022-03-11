c80358561d2df34f95c8fed169a0dcf9b40a5f3f
cooldev225 <developer225@hotmail.com>
2022-03-12 04:12:05 +0900

    cast temp

--- public/frontend/js/pages/message.js
+++ public/frontend/js/pages/message.js
@@ -108,8 +108,30 @@ $(document).ready(() => {
             success: function(res) {
                 let target = '#msgchatModal > div > div > div.modal-body > ul';
                 $(target).empty();
+
                 res.reverse().forEach(item => {
-                    addChatUserListItem(target, usersList.find(user => user.id == item.contact_id))
+                    let data = usersList.find(user => user.id == item.contact_id)
+                    $(target).prepend(
+                        `<li data-to="blank" key="${data.id}">
+                            <div class="chat-box">
+                            <div class="profile ${data.logout ? 'offline' : 'online'} bg-size" style="background-image: url(${data.avatar ? 'v1/api/downloadFile?path=' + data.avatar : "/images/default-avatar.png"}); background-size: cover; background-position: center center; display: block;">
+                                
+                            </div>
+                            <div class="details">
+                                <h5>${data.username}</h5>
+                                <h6>${data.description || 'Hello'}</h6>
+                            </div>
+                            <div class="date-status">
+                                <input class="form-check-input" type="checkbox" value="" aria-label="...">
+                            </div>
+                            </div>
+                        </li>`
+                    );
+                    if ($('#group_blank > .contact-details .media-body').find(`span[userId=${data.id}]`).length) {
+                        $(`#msgchatModal ul.chat-main li[key=${data.id}] input`).prop('checked', true);
+                        $(`#msgchatModal ul.chat-main li[key=${data.id}]`).addClass('active');
+                    }
+                    // addChatUserListItem(target, usersList.find(user => user.id == item.contact_id))
                 });
 
             },
@@ -119,17 +141,23 @@ $(document).ready(() => {
         });
     });
 
-    $('#msgchatModal ul.chat-main').on('click', 'li', (e) => {
-        // console.log(e.currentTarget);
-        let userId = $(e.currentTarget).attr('key');
-        console.log($(e.currentTarget).attr('key'));
-        if ($('#group_blank > .contact-details .media-body').find(`span[userId=${userId}]`).length) {
-            return;
+    $('#msgchatModal ul.chat-main').on('click', 'li', function(e) {
+        $(this).find('.form-check-input').prop('checked', !$(this).find('.form-check-input')[0].checked);
+        $(this).toggleClass('active');
+
+        let userId = $(this).attr('key');
+        if ($(this).find('.form-check-input').is(':checked')) {
+
+            if ($('#group_blank > .contact-details .media-body').find(`span[userId=${userId}]`).length) {
+                return;
+            }
+            let userName = $(this).find('.details h5').text();
+            $('#group_blank > .contact-details .media-body').append(`
+                <span userId=${userId}>${userName}&nbsp&nbsp<b>\u2716</b></span>
+            `);
+        } else {
+            $('#group_blank > .contact-details .media-body').find(`span[userId=${userId}]`).remove();
         }
-        let userName = $(e.currentTarget).find('.details h5').text();
-        $('#group_blank > .contact-details .media-body').append(`
-            <span userId=${userId}>${userName}&nbsp&nbsp<b>\u2716</b></span>
-        `);
     });
 
     $('#group_blank > .contact-details .media-body').on('click', 'span', function() {
