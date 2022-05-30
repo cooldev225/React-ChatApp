23b8eb3fed6e237acddf367a9af379620d7b958d
cooldev225 <developer225@hotmail.com>
2022-05-30 08:53:03 +0800

    group update

--- socket/app.js
+++ socket/app.js
@@ -525,7 +525,7 @@ io.on('connection', (socket) => {
 
     socket.on('createGroup', data => {
         console.log(data);
-        db.query(`INSERT INTO groups (title, users, type, owner) VALUES ("${data.title}", "${data.users}", 2, ${currentUserId})`, (error, item) => {
+        db.query(`INSERT INTO \`groups\` (title, users, type, owner) VALUES ("${data.title}", "${data.users}", 2, ${currentUserId})`, (error, item) => {
             console.log(item);
             data.id = item.insertId
             let senderSocketId = user_socketMap.get(currentUserId.toString());
